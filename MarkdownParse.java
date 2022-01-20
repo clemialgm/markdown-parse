00fcebf0c3106e4c52063e31e31a3a0c985208dd
clemialgm <j3guan@ucsd.edu>
2022-01-20 10:32:50 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            /*
+            
+            */
             currentIndex = closeParen + 1;
         }
         return toReturn;
