99f1522ac6b652868c42fa6e92f606bfbb83cab0
clemialgm <j3guan@ucsd.edu>
2022-02-24 21:42:49 -0800

    report4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,7 +12,7 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
+/*
     @Test
     public void testGetLinks() throws IOException {
         Path fileName = Path.of("test-file.md");
@@ -25,5 +25,27 @@ public class MarkdownParseTest {
         String contents = "[title]( space-in-url.com)";
         List<String> expect = List.of("space-in-url.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+    */
+
+    @Test
+    public void Snippet1() throws IOException{
+        Path fileName = Path.of("snippet1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("`google.com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void Snippet2() throws IOException{
+        Path fileName = Path.of("snippet2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("a.com","a.com(())","example.com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void Snippet3() throws IOException{
+        Path fileName = Path.of("snippet3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://ucsd-cse15l-w22.github.io/"), MarkdownParse.getLinks(contents));
     }
 }
